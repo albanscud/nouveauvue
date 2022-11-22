54c8b0b656004e1caea125de4d2249b33337d837
scud <alban.escudier@hotmail.fr>
2022-11-22 15:24:04 +0100

    nouveau router

--- nouveau-vue/src/App.vue
+++ nouveau-vue/src/App.vue
@@ -1,40 +1,36 @@
 
 
 <template>
-  <router-view/>
   
   <tetedepage/>
-  <corppageacceuil/>
+
+  <router-view/>
+
   <pieddepage/>
      
  </template> 
   
 
-  
- 
-  
- 
-
-
-
 <script>
+
 import tetedepage from './components/tetedepage.vue'
 import pieddepage from './components/pieddepage.vue'
-import corppageacceuil from './components/corppageacceuil.vue'
+
 
 export default {
   name: 'App',
   components: {
     tetedepage,
-    pieddepage,
-    corppageacceuil
+    pieddepage
   }
 }
+</script> 
+ 
+  
+ 
 
 
 
 
-
-</script>
 
 
