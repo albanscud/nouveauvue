d30420a3efe77d786f18bdb81dbd6a0d3d0caa0b
scud <alban.escudier@hotmail.fr>
2022-11-24 15:20:48 +0100

    ajout fichier json

--- nouveau-vue/src/components/infoscryptos.vue
+++ nouveau-vue/src/components/infoscryptos.vue
@@ -10,36 +10,33 @@
     </div>
     </div>  -->
   
-    <!-- <p class="py-5 text-center text-xl"> Différentes cryptomonnaies ci dessous : Voici le cours en temps réel du bitcoin et de l'ethereum.</p>
-  <div  class="">vvvvvgv
-    <div id="app">
-      <div class="justify-center pr-5">
+    <!-- <p class="py-5 text-center text-xl"> Différentes cryptomonnaies ci dessous : </p>
+      <div>
         <div class="text-center">
-          <div class="py-4" v-for="produit in produits" >
-            <p class="border-violet-600 border-4">
-              {{produit.name}} 
-            </p>
+          <div class="py-4" v-for="produit in produits" :key="produit.id" >
+            <p>{{produit.name}}</p>
+            <div>{{produit.creator}}</div>
+            <img :src="produit.image" alt="image">
           </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
-
- <div class="bg-white">
+          </div>
+      </div>  -->
+          
+  <div class="bg-white">
     <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 class="sr-only">Products</h2>
+      <h2 class="sr-only">Produits</h2>
 
       <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <a v-for="product in products" :key="product.id" :href="product.href" class="group">
+        <a v-for="produit in produits" :key="produit.id" :href="produit.href" class="group">
           <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-            <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center group-hover:opacity-75" />
+            <img :src="produit.image" :alt="produit.imageAlt" class="h-full w-full object-cover object-center group-hover:opacity-75" />
           </div>
-          <h3 class="mt-4 text-sm text-gray-700">{{ product.name }}</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">{{ product.price }}</p>
+          <h3 class="mt-4 text-sm text-gray-700">{{ produit.name }}</h3>
+          <h4 class="text-sm text-gray-700">{{ produit.creator }}</h4>
+          <p class="mt-1 text-lg font-medium text-gray-900">{{ produit.price }} {{produit.about}}</p>
         </a>
       </div>
     </div>
-  </div> 
+  </div>  
 
 
   
@@ -48,11 +45,13 @@
 
 
 
- <script setup>
-const products = [
+ <script>
+
+/* const products = [
   {
     id: 1,
     name: 'Bitcoin',
+    creator: 'Satoshi Nakamoto',
     href: '#',
     price: '1 BTC ≃ 16000 euros',
     imageSrc: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=023',
@@ -61,6 +60,7 @@ const products = [
   {
     id: 2,
     name: 'Ethereum',
+    creator: 'Vitalik Buterin',
     href: '#',
     price: '1 ETH ≃ 1200 euros',
     imageSrc: 'https://cryptologos.cc/logos/ethereum-eth-logo.png?v=023',
@@ -69,6 +69,7 @@ const products = [
   {
     id: 3,
     name: 'Cardano',
+    creator: 'Charles Hoskinson',
     href: '#',
     price: '1 ADA ≃ 0,30 euros',
     imageSrc: 'https://cryptologos.cc/logos/cardano-ada-logo.png?v=023',
@@ -77,6 +78,7 @@ const products = [
   {
     id: 4,
     name: 'Solana',
+    creator: 'Anatoly Yakovenko',
     href: '#',
     price: '1 SOL ≃ 13 euros',
     imageSrc: 'https://cryptologos.cc/logos/solana-sol-logo.png?v=023',
@@ -85,6 +87,7 @@ const products = [
   {
     id: 5,
     name: 'XRP',
+    creator: 'Ripple',
     href: '#',
     price: '1 XRP ≃ 0,30 euros',
     imageSrc: 'https://cryptologos.cc/logos/xrp-xrp-logo.png?v=023',
@@ -93,6 +96,7 @@ const products = [
   {
     id: 6,
     name:'BNB',
+    creator: 'Binance',
     href: '#',
     price: '1 BNB ≃ 280 euros',
     imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/5/57/Binance_Logo.png',
@@ -101,6 +105,7 @@ const products = [
   {
     id: 7,
     name: 'Dogecoin',
+    creator: 'Elon Musk',
     href: '#',
     price: '1 DOGE ≃ 0,05 euros',
     imageSrc: 'https://cryptologos.cc/logos/dogecoin-doge-logo.png?v=023',
@@ -109,37 +114,32 @@ const products = [
   {
     id: 8,
     name: 'Polkadot',
+    creator: 'Gavin Wood',
     href: '#',
     price: '1 DOT ≃ 5 euros',
     imageSrc: 'https://cryptologos.cc/logos/polkadot-new-dot-logo.png?v=023',
     imageAlt: 'photo polkadot',
   }
-]
+ ]  */
 
 
-/* import axios from 'axios'
+import axios from 'axios'
 
 export default {
-  props: {
-    id : Number,
-    image: string,
-    name: string,
-    creator: string,
-  },
   
   data() {
-    
-    return {
-      produits: []
+      return {
+      produits: [],
+      error: []
     }
   },
   created() {
     axios
-      .get('http://localhost:8080/produit.json')
+      .get(`http://localhost:8081/produit.json`)
       .then(response => {this.produits= response.data.produits})
       .catch(e => {this.errors.push(e)})
    }  
-} */
+}
 </script>
  
 
